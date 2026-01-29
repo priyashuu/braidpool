4744654c56ddf2eff1cc4c338554c0d57d55f452
Priya <150767072+priyashuu@users.noreply.github.com>
2026-01-29 23:48:15 +0530

    Update BitcoinStats.tsx

--- dashboard/src/components/BitcoinStats/BitcoinStats.tsx
+++ dashboard/src/components/BitcoinStats/BitcoinStats.tsx
@@ -3,7 +3,11 @@ import Prices from './Prices';
 const BitcoinStats = () => {
   return (
     <div>
+      <div ref={rewardsChartRef} className="w-full h-[90%]">
+            <ResponsiveContainer width="100%" height="100%">
       <Prices />
+            </ResponsiveContainer>
+      </div>
     </div>
   );
 };
